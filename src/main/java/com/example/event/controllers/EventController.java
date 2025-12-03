57d2607285c41ad3ee9410487002b911b5017a85
amine <aminebichiou2@gmail.com>
2025-12-03 11:03:09 +0100

    new test

--- src/main/java/com/example/event/controllers/EventController.java
+++ src/main/java/com/example/event/controllers/EventController.java
@@ -35,11 +35,12 @@ public class EventController {
         return eventService.updateEvent(event);
     }
 
-    
+
     @DeleteMapping("/{id}")
     public void deleteEvent(@PathVariable Long id) {
         eventService.deleteEvent(id);
     }
+    
 
 
 }
